02ed4f1d5ac595e38d6c1830e73862bd861599ef
madeleine2086 <>
2023-10-11 13:32:49 +0000

    create variables before creatinf check winner function

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const play = () => {
     let playerResult = document.getElementById('player-result');
     let computerResult = document.getElementById('computer-result');
     let resultSection = document.getElementById('result');
+    let userScoreDisplay = document.getElementById('player-score');
+    let computerScoreDisplay = document.getElementById('computer-score');
+    let userScore = 0;
+    let computerScore = 0;
     let computerChoice = "";
     let userChoice = "";
     let time = 3;
@@ -17,6 +21,7 @@ const play = () => {
                     clearInterval(timeInterval);
                     let computer = Math.floor(Math.random() * 3);
                     playerAndComputerDisplay(user.id, allButtons[computer].id)
+                    checkWinner(user.id, allButtons[computer].id);
                     resultSection.classList.add('result');
                 } else {
                     winner.textContent = time;
@@ -43,8 +48,13 @@ const play = () => {
 
         user.classList.add('red');
         resultSection.classList.remove('result');
+    }
+    
+    checkWinner = (user, computer) => {
 
-    }    
+    }
+
+
     startPlaying()
 
 }
